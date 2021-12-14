d1c881e76d5e8839faaad9f2dbfd5f6696cd3457
Martin Vysny <mavi@vaadin.com>
2021-12-14 14:05:53 +0200

    Move GpioPin to its own file

--- src/nativeMain/kotlin/Utils.kt
+++ src/nativeMain/kotlin/Utils.kt
@@ -26,19 +26,8 @@ fun Closeable.closeQuietly() {
 }
 
 /**
- * Represents a Raspberry PI GPIO pin. This number doesn't correspond to the physical
- * pin number. For example, GPIO pin 17 (GPIO17) is physical
- * pin number 11 (at least on on Raspberry PI 3B).
- * See https://www.raspberrypi.org/documentation/computers/os.html#gpio-pinout for more details.
- */
-typealias GpioPin = Int
-
-internal fun GpioPin.requireInGpioRange() {
-    require(this in 0..27) { "Invalid gpio number $this: must be 0..27" }
-}
-
-/**
- * Reads lines from stdin. The sequence ends once a blank line is received.
+ * Reads lines from stdin. The sequence ends once a blank line is received (user
+ * simply presses Enter without typing anything).
  */
 internal fun stdinLines(): Sequence<String> = generateSequence {
     var line = readLine(); if (line.isNullOrBlank()) line = null; line
